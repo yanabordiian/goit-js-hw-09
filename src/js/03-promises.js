ca453b8e4096107c9ce2f1b38b275233208c4576
yanabordiian <98254244+yanabordiian@users.noreply.github.com>
2023-09-04 17:27:36 +0200

    fixed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+import Notiflix from "notiflix";
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('form').addEventListener('submit', async function (e) {
+  e.preventDefault();
+  const delay = parseInt(this.elements.delay.value);
+  const step = parseInt(this.elements.step.value);
+  const amount = parseInt(this.elements.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const currentDelay = delay + i * step;
+    
+    try {
+      const result = await createPromise(position, currentDelay);
+      Notiflix.Notify.success(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`);
+    } catch (error) {
+      Notiflix.Notify.failure(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+    }
+  }
+});
