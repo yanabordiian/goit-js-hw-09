1aa1c2f086a331796f659b623e8894287595c4da
yanabordiian <98254244+yanabordiian@users.noreply.github.com>
2023-09-06 22:32:28 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,33 +1,49 @@
 import Notiflix from "notiflix";
 
+const refs = { 
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+}
+let delay = Number(refs.delay.value);
+let step = Number(refs.step.value);
+let amount = Number(refs.amount.value);
+let position = 0;
+
+refs.form.addEventListener('submit', onHandleForm);
+
+function onHandleForm(e) {
+  e.preventDefault();
+  
+  for (let i = 1; i <= amount; i += 1) {
+    position = i;
+    createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  delay += step;
+  }
+  refs.form.reset();
+}
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
+
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
-      } else {
-        reject({ position, delay });
       }
+
+      reject({ position, delay });
     }, delay);
   });
 }
 
-document.querySelector('form').addEventListener('submit', async function (e) {
-  e.preventDefault();
-  let delay = parseInt(this.elements.delay.value);
-  let step = parseInt(this.elements.step.value);
-  const amount = parseInt(this.elements.amount.value);
 
-  for (let i = 0; i < amount; i+= 1) {
-    const position = i + 1;
-    
-    try {
-      const result = await createPromise(position, currentDelay);
-      Notiflix.Notify.success(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`);
-    } catch (error) {
-      Notiflix.Notify.failure(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
-    }
-    delay += step;
-  }
-});
+
+
